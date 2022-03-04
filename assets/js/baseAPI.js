a29b5c785dc63cd366af5bb985556666ae7c3a24
fengzimo <1035667145@qq.com>
2022-03-04 23:38:10 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,21 @@
 $.ajaxPrefilter(function(options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+    if (options.url.indexOf('/my/') != -1) {
+        // 统一为有权限的接口设置headers请求头
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 全局统一挂载complete回调函数
+    // 控制用户访问权限 用户不登陆不能访问后台首页
+    options.complete = function(res) {
+        // 在complete回调函数中使用responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.清空本地存储中的token
+            localStorage.removeItem('token');
+            // 2.跳转到登录页面
+            location.href = 'login.html';
+        }
+    }
 });
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+$(function() {
+    // 获取用户基本信息
+    getUserInfo();
+    // 退出功能
+    $('#btn-ogout').on('click', function() {
+        layui.layer.confirm('确定退出登录', { icon: 3, title: '提示' }, function(index) {
+            // 当用户点击确定时退出
+            // 1.清空本地存储中的token
+            localStorage.removeItem('token');
+            // 2.返回登录页面
+            location.href = 'login.html';
+            // 关闭弹出层
+            layer.close(index);
+        });
+    });
+
+});
+
+// 获取用户信息
+function getUserInfo() {
+    $.ajax({
+        method: 'get',
+        url: '/my/userinfo',
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败');
+            }
+            renderAvatar(res.data);
+        }
+    });
+}
+
+// 渲染用户的头像
+function renderAvatar(user) {
+    // 获取用户的用户名
+    var name = user.nickname || user.username;
+    $('#welcom').html('欢迎&nbsp&nbsp' + name);
+    // 按需渲染用户头像
+    if (user.user_pic !== null) {
+        // 用户有头像 渲染用户头像
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        // 用户没有头像，渲染文字头像
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+    }
+}
